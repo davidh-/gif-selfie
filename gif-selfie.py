ac0cdab3c53326f1af2820cb4bd24948515fb3f0
David Dominguez Hooper <davidhooper4@gmail.com>
2020-04-15 02:38:49 -0700

    done, working

--- gif-selfie.py
+++ gif-selfie.py
@@ -1,7 +1,8 @@
-import selenium, os
+import selenium, os, base64, time
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 def select(arg1, arg2):
 	select_element = Select(driver.find_element_by_id(arg1))
@@ -17,6 +18,21 @@ def change_entry(arg1, arg2, arg3, arg4):
 			element.send_keys(no_bits)
 	else:
 		element.send_keys(arg3)
+
+
+
+def get_image(file_num):
+	#get and download image
+	output = driver.find_element_by_id('output')
+	# get the canvas as a PNG base64 string
+	canvas_base64 = driver.execute_script('return arguments[0].toDataURL(\'image/png\').substring(21);', output)
+
+	# decode
+	canvas_png = base64.b64decode(canvas_base64)
+
+	# save to a file
+	with open(r'frame_' + file_num + '.png', 'wb') as f:
+	    f.write(canvas_png)
 
 backspace = '\b'
 backspace_three = backspace*3
@@ -43,9 +59,15 @@ for i in range(1, 5):
 
 select('pixelPlaneSelect', pixelPlane)
 
-curDir = os.getcwd()
-driver.find_element_by_id('fileName').send_keys(curDir + '/frame-video8-1.raw')
+
+for i in range(1, 100):
+	i_str = str(i)
+	curDir = os.getcwd()
+	driver.implicitly_wait(10)
+	driver.find_element_by_id('fileName').send_keys(curDir + '/frame-video8-' + i_str + '.raw')
+	print("Getting frame: " + i_str)
+	time.sleep(.5)
+	get_image(i_str)
 
 
-
-# driver.quit()
+driver.quit()
